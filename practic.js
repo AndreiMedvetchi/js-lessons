4eef549dd888a63f3a01d9125a5dd9406de58e28
Andrei <andreipanda@bk.ru>
2023-11-06 00:31:17 +0200

    Start future

---
+++ practic.js
@@ -0,0 +1,17 @@
+const numberOFfilms = +prompt('Сколько фильмов вы уже посмотрели','');
+
+const personalMovieDB = {
+    count:numberOFfilms,
+    movies : {},
+    actors: {},
+    genres : false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?',''),
+      c = prompt('Один из последних просмотренных фильмов?',''),
+      d = prompt('На сколько оцените его','');
+ personalMovieDB.movies[a]=b;
+ personalMovieDB.movies[c]=d;
+
+ console.log(personalMovieDB.movies);
